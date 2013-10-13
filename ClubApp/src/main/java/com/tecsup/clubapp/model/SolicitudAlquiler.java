f04a61286b7775be7a1cc6f9ce74429919187362
LAB704-00 <LAB704-00@LAB704-08>
2013-10-13 11:37:34 -0500

    Model David CON GETTER

--- ClubApp/src/main/java/com/tecsup/clubapp/model/SolicitudAlquiler.java
+++ ClubApp/src/main/java/com/tecsup/clubapp/model/SolicitudAlquiler.java
@@ -11,15 +11,79 @@ import java.sql.Date;
  * @author LAB704-00
  */
 public class SolicitudAlquiler {
-    private long id;
-private String horaInicio;
-private String horaFin;
-private Date dia;
-private String servicios;
-private int estado;
-private Socio idSocio;
-private Campo long idCampo;
 
-    
-    
+    private long id;
+    private String horaInicio;
+    private String horaFin;
+    private Date dia;
+    private String servicios;
+    private int estado;
+    private Socio idSocio;
+    private Campo idCampo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(String horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public Date getDia() {
+        return dia;
+    }
+
+    public void setDia(Date dia) {
+        this.dia = dia;
+    }
+
+    public String getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(String servicios) {
+        this.servicios = servicios;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Socio getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(Socio idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public Campo getIdCampo() {
+        return idCampo;
+    }
+
+    public void setIdCampo(Campo idCampo) {
+        this.idCampo = idCampo;
+    }
+
+
 }
