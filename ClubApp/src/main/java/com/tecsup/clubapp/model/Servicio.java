26be42430e1bb5bfd6250c1d6de4cac6225fc05a
LAB704-00 <LAB704-00@LAB704-08>
2013-10-13 11:30:20 -0500

    Model David

---
+++ ClubApp/src/main/java/com/tecsup/clubapp/model/Servicio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecsup.clubapp.model;
+
+/**
+ *
+ * @author LAB704-00
+ */
+public class Servicio {
+    private long id;
+    private String descripcion;
+    private Double costoHora;
+
+    
+    
+}
---
+++ ClubApp/src/main/java/com/tecsup/clubapp/model/SolicitudAlquiler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecsup.clubapp.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author LAB704-00
+ */
+public class SolicitudAlquiler {
+    private long id;
+private String horaInicio;
+private String horaFin;
+private Date dia;
+private String servicios;
+private int estado;
+private Socio idSocio;
+private Campo long idCampo;
+
+    
+    
+}
